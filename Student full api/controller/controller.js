b02e7a0d10d9d8bc05ebd9cd76db6f134cb159d6
bancmark <codeweb66@gmail.com>
2025-12-03 21:10:36 +0530

    1

--- Student full api/controller/controller.js
+++ Student full api/controller/controller.js
@@ -15,8 +15,8 @@ export const addStudent = async (req, res) => {
 export const viewStudent = async (req, res) => {
     try {
         const stid = req.params.Classid
-        const Finder = await studentSchema.find({id: stid})
-        
+        const Finder = await studentSchema.find({ id: stid })
+
         await res.send(Finder)
     }
     catch (err) {
@@ -24,13 +24,101 @@ export const viewStudent = async (req, res) => {
     }
 }
 
-export const ByMongoose = async(req, res)=>{
-    try{
+export const ByMongoose = async (req, res) => {
+    try {
         const stId = req.params.Mongooseid
         const Finder = await studentSchema.findById(stId)
         await res.send(Finder)
     }
-    catch(err){
+    catch (err) {
         res.send(err)
+    }
+}
+
+export const Byname = async (req, res) => {
+    try {
+        const Qname = req.query.name
+        const NFinder = await studentSchema.find({ name: new RegExp(Qname, 'i') })
+        await res.send(NFinder)
+    }
+    catch (err) {
+        res.send(err)
+    }
+}
+
+export const Update = async (req, res) => {
+    try {
+        const id = req.params.id
+        const UpdatedData = req.body
+
+        // Determine filter (MongoId or custom id)
+        let filter = {}
+        if (id.length === 24 && /^[a-fA-F0-9]+$/.test(id)) {
+            filter = { _id: id }
+        }
+        else {
+            filter = { id: id }
+        }
+
+
+        const Updater = await studentSchema.findOneAndUpdate(
+            filter,
+            UpdatedData,
+            { new: true }
+        )
+
+        if (!Updater) {
+            res.status(404).send("ome thind is woring")
+        }
+
+        res.send(Updater)
+        console.log(Updater);
+    }
+    catch (err) {
+        res.send(err)
+    }
+}
+
+export const Delete = async (req, res) => {
+    try {
+        const id = req.params.id
+
+        // Determine filter (MongoId or custom id)
+        let filter = {}
+        if (id.length === 24 && /^[a-fA-F0-9]+$/.test(id)) {
+            filter = { _id: id }
+        }
+        else {
+            filter = { id: id }
+        }
+
+
+        const deleter = await studentSchema.findOneAndDelete(filter)
+        if (!deletedStudent) {
+            return res.status(404).send("No student found with this ID");
+        }
+        res.send({
+            msg: "iteam is deleed",
+            dle: deleter
+        })
+    }
+    catch (err) {
+        res.send(err)
+    }
+}
+
+
+export const pagination = async (req, res) => {
+    try {
+        const page = parseInt(req.query.p) || 1
+        const limit = parseInt(req.query.l) || 5
+        const skip = (page - 1) * limit;
+        const finder = await studentSchema.find()
+            .skip(skip)
+            .limit(limit)
+        res.send(finder);
+    }
+    catch (err) {
+        res.status(400).send(err);
     }
 }
--- Student full api/router/router.js
+++ Student full api/router/router.js
@@ -1,7 +1,16 @@
 import express from 'express'
-import {addStudent, viewStudent, ByMongoose} from '../controller/controller.js'
+import {addStudent, viewStudent, ByMongoose, Byname, Update, Delete, pagination} from '../controller/controller.js'
 const router = express.Router()
 
+const protection = (req, res, next)=>{
+    const pass = req.query.pass
+    if ( pass === 'pass'){
+        next()
+    }
+    else{
+        res.status(400).send("Invalid password")
+    }
+}
 
 
 router.get('/', (req, res)=>{
@@ -10,6 +19,11 @@ router.get('/', (req, res)=>{
 
 router.post('/add', addStudent)
 router.get('/get/class/:Classid', viewStudent)
+router.get('/get', Byname)
 router.get('/get/mongoose/:Mongooseid', ByMongoose)
+router.get('/list', pagination)
+
+router.put('/update/:id', protection, Update)
+router.delete('/delete/:id', protection, Delete)
 
 export default router
