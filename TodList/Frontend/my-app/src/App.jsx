d8ad998949570b59a338121341d1840e41e99bf3
bancmark <codeweb66@gmail.com>
2026-02-02 15:49:59 +0530

    1

--- TodList/Frontend/my-app/src/App.jsx
+++ TodList/Frontend/my-app/src/App.jsx
@@ -5,9 +5,11 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      
-    </>
+    <div className='w-screen h-screen bg-[#242424] overflow-hidden text-white'>
+      <div className='bg-[#6e6868] my-3 mx-3 rounded-md'>
+        <h1 className='text-2xl py-4 my-4 px-1 font-bold border-2 w-1/2'>iTask</h1>
+      </div>
+    </div>
   )
 }
 
