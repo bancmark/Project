271c3733cf947dd70258b3811f329150db90a201
bancmark <codeweb66@gmail.com>
2026-02-03 17:02:02 +0530

    1

--- TodList/Frontend/my-app/src/App.jsx
+++ TodList/Frontend/my-app/src/App.jsx
@@ -6,8 +6,11 @@ function App() {
 
   return (
     <div className='w-screen h-screen bg-[#242424] overflow-hidden text-white'>
-      <div className='bg-[#6e6868] my-3 mx-3 rounded-md'>
-        <h1 className='text-2xl py-4 my-4 px-1 font-bold border-2 w-1/2'>iTask</h1>
+      <div className='bg-[#6e6868] my-3 mx-3 px-2 py-2 rounded-md justify-center'>
+        <h1 className='text-2xl py-2 my-4 px-1 mx-3 font-bold border-2 w-1/5 text-center rounded-md'>iTask</h1>
+      </div>
+      <div className='bg-[#6e6868] my-3 mx-3 px-2 py-2 rounded-md justify-center'>
+        <input className='bg-transparent border-b-2 border-[#242424] w-1/4 text-white placeholder-white focus:outline-none' type="text" placeholder='Enter your task' />
       </div>
     </div>
   )
