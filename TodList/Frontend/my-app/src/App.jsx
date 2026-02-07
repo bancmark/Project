6d4b3f3ef955d549ff856517cd8adec522a58e94
bancmark <codeweb66@gmail.com>
2026-02-07 15:37:13 +0530

    rendering task

--- TodList/Frontend/my-app/src/App.jsx
+++ TodList/Frontend/my-app/src/App.jsx
@@ -6,10 +6,18 @@ function App() {
   const [Task, setTask] = useState("")
   const [Tasks, setTasks] = useState([])
 
+  useEffect(() => {
+    const AlreadyTask = localStorage.getItem("task")
+    if(AlreadyTask){
+      setTasks(JSON.parse(AlreadyTask))
+    }
+  }, [])
+  
+
   const add = ()=>{
     if(Task.trim() === ""){return}//if task is return it
 
-    const newTask = [...Tasks, Task]//stroing task from task to array
+    const newTask = [...Tasks, Task]//storing task from task to array
     setTasks(newTask)
 
     localStorage.setItem("task", JSON.stringify(newTask))//store in localstorage
@@ -18,18 +26,25 @@ function App() {
     //checking everything is okay
     console.log(Task)
     console.log(Tasks)
-    console.log(Task)
+    console.log(newTask)
 
   }
 
   return (
     <div className='w-screen h-screen bg-[#242424] overflow-hidden text-white'>
-      <div className='bg-[#6e6868] my-3 mx-3 px-2 py-2 rounded-md justify-center'>
+      <div className='commonDiv hover:cursor-pointer'>
         <h1 className='text-2xl py-2 my-4 px-1 mx-3 font-bold border-2 w-1/5 text-center rounded-md'>iTask</h1>
       </div>
-      <div className='bg-[#6e6868] my-3 mx-3 px-4 pr-10 py-2 flex rounded-md justify-center gap-10'>
+
+      <div className=' commonDiv pr-10 flex gap-10'>
         <input className='inputt' type="text" value={Task} onChange={(e=>{setTask(e.target.value)})} placeholder='Enter your task' />
         <button className='hover:cursor-pointer border-amber-950 rounded-md px-2 bg-amber-900 justify-between whitespace-nowrap' onClick={add}>Add</button>
+      </div>
+
+      <div className='commonDiv gap-5'>
+        {Tasks.map((item, index)=>(
+          <div key={index} className='text-amber-50 mx-10'>{item}</div>
+        ))}
       </div>
     </div>
   )
