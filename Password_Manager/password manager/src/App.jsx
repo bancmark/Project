ecde86b26dfea4d32de9bfc8d83c83e0eae4d7e7
bancmark <codeweb66@gmail.com>
2026-03-29 16:13:30 +0530

    navbar

--- Password_Manager/password manager/src/App.jsx
+++ Password_Manager/password manager/src/App.jsx
@@ -1,15 +1,19 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import './App.css'
+import Navbar from './components/Navbar.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <Navbar />
+
+      <div className="relative h-screen w-full bg-slate-950 overflow-hidden">
+        <div className="absolute bottom-0 left-[-20%] right-0 top-[-10%] h-[500px] w-[500px] rounded-full bg-[radial-gradient(circle_farthest-side,rgba(255,0,182,.15),rgba(255,255,255,0))]"></div>
+        <div className="absolute bottom-0 right-[-20%] top-[-10%] h-[500px] w-[500px] rounded-full bg-[radial-gradient(circle_farthest-side,rgba(255,0,182,.15),rgba(255,255,255,0))]"></div>
+      </div>
+      {/* <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]"></div> */}
     </>
   )
 }
