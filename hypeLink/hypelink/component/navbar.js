1e81b9f98f7b30018faf8ff6239ef9f16a98bf46
bancmark <codeweb66@gmail.com>
2026-05-15 15:38:24 +0530

    navbar added

---
+++ hypeLink/hypelink/component/navbar.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <>
+        <nav>
+            <ul className='flex'>
+                <li>Home</li>
+                <li>About</li>
+                <li>Shortner</li>
+                <li>Contact</li>
+                <li>Github</li>
+            </ul>
+        </nav>
+        </>
+    )
+}
+
+export default Navbar
