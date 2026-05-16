11cb361a86d8979ce088834cfd1f0e4bba637092
bancmark <codeweb66@gmail.com>
2026-05-16 15:41:42 +0530

    navbar modify

--- hypeLink/hypelink/app/layout.js
+++ hypeLink/hypelink/app/layout.js
@@ -23,8 +23,36 @@ export default function RootLayout({ children }) {
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <Navbar/>
-      <body className="min-h-full flex flex-col">{children}</body>
+
+      <body className="min-h-screen bg-slate-950">
+
+        <div className="relative min-h-screen w-full bg-slate-950">
+
+          {/* Background Grid */}
+          <div className="absolute inset-0 
+      bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),
+      linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] 
+      bg-[size:14px_24px]
+      [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)]">
+          </div>
+
+          {/* Main Content */}
+          <div className="relative z-10">
+            <Navbar />
+            {children}
+          </div>
+
+        </div>
+
+      </body>
+      {/* <body className="min-h-full flex flex-col">
+
+        <div classNamw="relative h-full w-full bg-slate-950"><div className="absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)]"></div></div>
+        <Navbar />
+        {children}
+      </body> */}
     </html>
   );
 }
+
+
--- hypeLink/hypelink/component/navbar.js
+++ hypeLink/hypelink/component/navbar.js
@@ -3,13 +3,13 @@ import React from 'react'
 const Navbar = () => {
     return (
         <>
-        <nav>
-            <ul className='flex'>
-                <li>Home</li>
-                <li>About</li>
-                <li>Shortner</li>
-                <li>Contact</li>
-                <li>Github</li>
+        <nav className=''>
+            <ul className='flex gap-3 justify-end p-4 bg-purple-600'>
+                <li className='cursor-pointer scale-110'>Home</li>
+                <li className='cursor-pointer'>About</li>
+                <li className='cursor-pointer'>Shortner</li>
+                <li className='cursor-pointer'>Contact</li>
+                <li className='cursor-pointer'>Github</li>
             </ul>
         </nav>
         </>
