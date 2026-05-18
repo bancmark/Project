38ad422ca7a54f113e7237599b2230dad016d5f8
bancmark <codeweb66@gmail.com>
2026-05-18 14:11:59 +0530

    navabr fix andd home page layout star

--- hypeLink/hypelink/component/navbar.js
+++ hypeLink/hypelink/component/navbar.js
@@ -3,15 +3,15 @@ import React from 'react'
 const Navbar = () => {
     return (
         <>
-        <nav className='m-4'>
-            <ul className='flex gap-6 justify-end p-5 bg-purple-600 rounded-2xl'>
-                <li className='nav_li'>Home</li>
-                <li className='nav_li'>About</li>
-                <li className='nav_li'>Shortner</li>
-                <li className='nav_li'>Contact</li>
-                <li className='nav_li'>Github</li>
-            </ul>
-        </nav>
+            <nav className='m-4'>
+                <ul className='flex gap-6 justify-end p-5 bg-purple-600 rounded-2xl'>
+                    <li className='nav_li'>Home</li>
+                    <li className='nav_li'>About</li>
+                    <li className='nav_li'>Shortner </li>
+                    <li className='nav_li'>Contact</li>
+                    <li className='nav_li'>Github</li>
+                </ul>
+            </nav>
         </>
     )
 }
