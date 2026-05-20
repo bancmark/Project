eea3935d520d7aaa3a01a7ac9a48d314c10dbda0
bancmark <codeweb66@gmail.com>
2026-05-20 15:16:03 +0530

    homepage is completed and shoturl is added

--- hypeLink/hypelink/component/navbar.js
+++ hypeLink/hypelink/component/navbar.js
@@ -1,13 +1,14 @@
 import React from 'react'
+import Link from "next/link"
 
 const Navbar = () => {
     return (
         <>
             <nav className='m-4'>
                 <ul className='flex gap-6 justify-end p-3 bg-purple-600 rounded-2xl'>
-                    <li className='nav_li'>Home</li>
+                    <Link href = "/" className='nav_li'>Home</Link>
                     <li className='nav_li'>About</li>
-                    <li className='nav_li'>Shortner </li>
+                    <Link href = "/shorturl" className='nav_li'>Shortner </Link>
                     <li className='nav_li'>Contact</li>
                     <li className='nav_li'>Github</li>
                 </ul>
