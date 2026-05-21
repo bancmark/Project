8e17ef58c92d663ab979c506a6008bfc7fa3b240
bancmark <codeweb66@gmail.com>
2026-05-21 14:26:08 +0530

    now UI is fix backend is going on

--- hypeLink/hypelink/app/shorturl/page.js
+++ hypeLink/hypelink/app/shorturl/page.js
@@ -2,9 +2,19 @@ import React from 'react'
 
 const page = () => {
     return (
-        <div className='text-white'>
-            it is shorturl page
-        </div>
+        <>
+            <div className='backdrop-blur-md bg-white/10 border border-white/20 rounded-2xl shadow-lg m-4 h-70 my-10'>
+                <div className='flex m-3 justify-between'>
+                    <input type="text" placeholder='Website' className='text-white  border rounded-2xl border-blue-700 w-full p-3 m-3' />
+                </div>
+                <div className='flex m-3 justify-between'>
+                    <input type="text" placeholder='Short URL' className='text-white  border rounded-2xl border-blue-700 w-full p-3 m-3' />
+                </div>
+                <div className='flex justify-center'>
+                    <button className='text-white hover:cursor-pointer  rounded-2xl border border-white p-4 px-6'>Enter</button>
+                </div>
+            </div>
+        </>
     )
 }
 
