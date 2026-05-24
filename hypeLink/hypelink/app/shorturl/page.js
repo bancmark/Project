c97a3dc18ab0c3138fc3d80db61777723e03335a
bancmark <codeweb66@gmail.com>
2026-05-24 16:02:44 +0530

    shorturl function some added

--- hypeLink/hypelink/app/shorturl/page.js
+++ hypeLink/hypelink/app/shorturl/page.js
@@ -1,18 +1,42 @@
+"use client"
 import React from 'react'
+import { useState } from 'react'
 
 const page = () => {
+    const [url, setUrl] = useState("")
+    const [shortUrl, setShortUrl] = useState("")
+    console.log(shortUrl, url);
+
+    async function genrate(params) {
+        
+    } 
     return (
         <>
             <div className='backdrop-blur-md bg-white/10 border border-white/20 rounded-2xl shadow-lg m-4 h-70 my-10'>
                 <div className='flex m-3 justify-between'>
-                    <input type="text" placeholder='Website' className='text-white  border rounded-2xl border-blue-700 w-full p-3 m-3' />
+                    <input
+                        type="text"
+                        placeholder='Website'
+                        className='text-white  border rounded-2xl border-blue-700 w-full p-3 m-3'
+                        value={url}
+                        onChange={(e => setUrl(e.target.value))}
+                    />
                 </div>
                 <div className='flex m-3 justify-between'>
-                    <input type="text" placeholder='Short URL' className='text-white  border rounded-2xl border-blue-700 w-full p-3 m-3' />
+                    <input
+                        type="text"
+                        placeholder='Short URL'
+                        className='text-white  border rounded-2xl border-blue-700 w-full p-3 m-3'
+                        value={shortUrl}
+                        onChange={(e => setShortUrl(e.target.value))} />
                 </div>
                 <div className='flex justify-center'>
-                    <button className='text-white hover:cursor-pointer hover:bg-purple-500 rounded-2xl border hover:border-transparent
-    hover:scale-105 border-white p-4 px-6 cursor-pointer'>Enter</button>
+                    <button
+                        className='text-white hover:cursor-pointer hover:bg-purple-500 rounded-2xl border hover:border-transparent
+    hover:scale-105 border-white p-4 px-6 cursor-pointer'
+                        onClick={genrate}>
+                        Enter
+                    </button>
                 </div>
             </div>
         </>
