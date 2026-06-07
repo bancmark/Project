61298d8b8eab96cbf5002c012a47f3900ac0e986
bancmark <codeweb66@gmail.com>
2026-06-07 14:32:57 +0530

    navbar final style is finish

--- linktree/components/navbar.js
+++ linktree/components/navbar.js
@@ -1,18 +1,23 @@
 import React from 'react'
+import Image from "next/image";
+import Logo from "@/public/66634daccb34e6d65a41c76d_download.svg"
 
 const Navbar = () => {
+    const menueItem = 'hover:bg-[#eff0ec] p-3 hover:cursor-pointer rounded-2xl'
     return (
-        <div className='bg-white flex m-7 p-7 rounded-4xl gap-6'>
-            <h2 className='text-black font-bold'>LinkTree</h2>
-            <ul className='flex text-black gap-6'>
-                <li>Product</li>
-                <li>Template</li>
-                <li>Marketplace</li>
-                <li>Learn</li>
-                <li>Pricing</li>
+        <div className='bg-white flex m-7 px-4 py-3 rounded-full gap-6'>
+            <Image src={Logo} alt="Logo" width={100} height={50} />
+            <ul className='flex text-black items-center justify-center'>
+                <li className={menueItem}>Product</li>
+                <li className={menueItem}>Template</li>
+                <li className={menueItem}>Marketplace</li>
+                <li className={menueItem}>Learn</li>
+                <li className={menueItem}>Pricing</li>
             </ul>
-            <button className='bg-gray-200'>Log in</button>
-            <button>Sign up free</button>
+            <div className='gap-4 ml-auto flex'>
+                <button className='bg-gray-100 text-black py-4 px-5 hover:cursor-pointer rounded-2xl hover:bg-[#e9e9e9]'>Log in</button>
+                <button className='bg-[#1e2330] text-white p-4 rounded-full hover:cursor-pointer hover:bg-[#262d3e]'>Sign up free</button>
+            </div>
         </div>
     )
 }
