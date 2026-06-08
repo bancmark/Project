6499a8fc219ccb3ff47bbe79c91ae859cc1a7b84
bancmark <codeweb66@gmail.com>
2026-06-08 15:36:30 +0530

    more thinks addded in homepage

--- linktree/app/page.js
+++ linktree/app/page.js
@@ -9,11 +9,11 @@ export default function Home() {
       <div className="bg-[#2665d6] min-h-[100vh]">
         hello2
       </div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
+      <div   className="bg-[#780016] min-h-[100vh]"></div>
+      <div className="bg-[#e8efd6] min-h-[100vh]"></div>
+      <div className="bg-[#f3f3f1] min-h-[150vh]"></div>
+      <div className="bg-[#780016] min-h-[100vh]"></div>
+      <div className="bg-[#502274] min-h-[100vh]"></div>
     </div>
   );
 }
