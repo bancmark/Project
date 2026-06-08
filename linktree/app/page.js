13d594971d7102e83785301906ac10059030beb1
bancmark <codeweb66@gmail.com>
2026-06-08 15:28:18 +0530

    navbar floating and homepage colour

--- linktree/app/page.js
+++ linktree/app/page.js
@@ -2,6 +2,18 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>Hello</div>
+    <div>
+      <div className="bg-[#d2e823] min-h-[100vh]">
+        hello
+      </div>
+      <div className="bg-[#2665d6] min-h-[100vh]">
+        hello2
+      </div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
   );
 }
--- linktree/components/navbar.js
+++ linktree/components/navbar.js
@@ -5,18 +5,20 @@ import Logo from "@/public/66634daccb34e6d65a41c76d_download.svg"
 const Navbar = () => {
     const menueItem = 'hover:bg-[#eff0ec] p-3 hover:cursor-pointer rounded-2xl'
     return (
-        <div className='bg-white flex m-7 px-4 py-3 rounded-full gap-6'>
-            <Image src={Logo} alt="Logo" width={100} height={50} />
-            <ul className='flex text-black items-center justify-center'>
-                <li className={menueItem}>Product</li>
-                <li className={menueItem}>Template</li>
-                <li className={menueItem}>Marketplace</li>
-                <li className={menueItem}>Learn</li>
-                <li className={menueItem}>Pricing</li>
-            </ul>
-            <div className='gap-4 ml-auto flex'>
-                <button className='bg-gray-100 text-black py-4 px-5 hover:cursor-pointer rounded-2xl hover:bg-[#e9e9e9]'>Log in</button>
-                <button className='bg-[#1e2330] text-white p-4 rounded-full hover:cursor-pointer hover:bg-[#262d3e]'>Sign up free</button>
+        <div className='fixed top-6 left-0 right-0 z-50 flex justify-center'>
+            <div className='bg-white flex px-6 py-3 rounded-full gap-6'>
+                <Image src={Logo} alt="Logo" width={100} height={50} />
+                <ul className='flex text-black items-center justify-center'>
+                    <li className={menueItem}>Product</li>
+                    <li className={menueItem}>Template</li>
+                    <li className={menueItem}>Marketplace</li>
+                    <li className={menueItem}>Learn</li>
+                    <li className={menueItem}>Pricing</li>
+                </ul>
+                <div className='gap-4 ml-auto flex'>
+                    <button className='bg-gray-100 text-black py-4 px-5 hover:cursor-pointer rounded-2xl hover:bg-[#e9e9e9]'>Log in</button>
+                    <button className='bg-[#1e2330] text-white p-4 rounded-full hover:cursor-pointer hover:bg-[#262d3e]'>Sign up free</button>
+                </div>
             </div>
         </div>
     )
