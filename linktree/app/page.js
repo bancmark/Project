f7abadc76395f5214ce7f20dec2a0354dbc40094
bancmark <codeweb66@gmail.com>
2026-06-09 11:58:38 +0530

    navabr, home page
    navbat more details has been added ann homepage work in progess

--- linktree/app/page.js
+++ linktree/app/page.js
@@ -3,13 +3,23 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <div className="bg-[#d2e823] min-h-[100vh]">
-        hello
+      <div className="bg-[#d2e823] min-h-[120vh] flex">
+        <div className="w-1/2 ml-20 mt-60 max-w-xl">
+          <div className="">
+            <h1 className="text-[#254f1a] text-7xl font-black ">A link in bio built for you.</h1>
+            <p className="text-[#354f1a] mt-3 font-semibold">Join 70M+ people using Linktree for their link in bio. One link to help you share everything you create, curate and sell from your Instagram, TikTok, Twitter, YouTube and other social media profiles</p>
+          </div>
+          <div className="felx">
+            <input type="text" className="bg-white text-black p-4 rounded-2xl mt-9 outline-none" />
+            <button className="p-5 px-9 bg-[#254f1a] rounded-full ml-2 font-semibold hover:cursor-pointer">Get started for free</button>
+          </div>
+        </div>
+
+        <div className="w-1/2"></div>
       </div>
-      <div className="bg-[#2665d6] min-h-[100vh]">
-        hello2
-      </div>
-      <div   className="bg-[#780016] min-h-[100vh]"></div>
+
+      <div className="bg-[#2665d6] min-h-[100vh]"></div>
+      <div className="bg-[#780016] min-h-[100vh]"></div>
       <div className="bg-[#e8efd6] min-h-[100vh]"></div>
       <div className="bg-[#f3f3f1] min-h-[150vh]"></div>
       <div className="bg-[#780016] min-h-[100vh]"></div>
--- linktree/components/navbar.js
+++ linktree/components/navbar.js
@@ -6,7 +6,7 @@ const Navbar = () => {
     const menueItem = 'hover:bg-[#eff0ec] p-3 hover:cursor-pointer rounded-2xl'
     return (
         <div className='fixed top-6 left-0 right-0 z-50 flex justify-center'>
-            <div className='bg-white flex px-6 py-3 rounded-full gap-6'>
+            <div className='bg-white flex px-6 py-3 rounded-full gap-10'>
                 <Image src={Logo} alt="Logo" width={100} height={50} />
                 <ul className='flex text-black items-center justify-center'>
                     <li className={menueItem}>Product</li>
@@ -15,7 +15,7 @@ const Navbar = () => {
                     <li className={menueItem}>Learn</li>
                     <li className={menueItem}>Pricing</li>
                 </ul>
-                <div className='gap-4 ml-auto flex'>
+                <div className='gap-4 ml-80 flex'>
                     <button className='bg-gray-100 text-black py-4 px-5 hover:cursor-pointer rounded-2xl hover:bg-[#e9e9e9]'>Log in</button>
                     <button className='bg-[#1e2330] text-white p-4 rounded-full hover:cursor-pointer hover:bg-[#262d3e]'>Sign up free</button>
                 </div>
