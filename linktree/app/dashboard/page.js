f7a62da5a041ecac5efbd31cc16fc67b13b41d87
bancmark <codeweb66@gmail.com>
2026-06-25 15:38:36 +0530

    dashboard under the process

--- linktree/app/dashboard/page.js
+++ linktree/app/dashboard/page.js
@@ -1,17 +1,35 @@
 // app/dashboard/page.js
 import { auth } from "@/auth"
 import { redirect } from "next/navigation"
+import Image from "next/image";
+import addition from '@/public/addition.png'
+
 
 export default async function Dashboard() {
     const session = await auth()
 
     if (!session) redirect("/")
 
-    return(
-        <div className="bg-white">
+    return (
+        <div className="bg-white flex">
+            <aside className="w-1/4 bg-white min-h-screen text-black m-5 ml-5">
+                <h1>{session.user?.name}</h1>
+                <div className=" mt-3">
+                    <div className="flex">
+                        <Image
+                            src={addition}
+                            alt="additon"
+                            height={20}
+                            width={20}
+                        />
+                        <p>My Linktree </p>
+                    </div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
+            </aside>
             <div></div>
-            <div></div>
-            <h1>Welcome, {session.user?.name}</h1>
         </div>
     )
 }
